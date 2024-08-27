ea178397142cf0c7081eb74743144d95342fa82f
Kostya <Onischenko411@gmail.com>
2024-08-27 17:14:27 +0300

    first commit

---
+++ src/components/List/List.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from "react";
+import './List.css'
+const animals = [
+  { type: `turtle`, icon: `🐢` },
+  { type: `octopus`, icon: `🐙` },
+  { type: `fish`, icon: `🐠` },
+  { type: `flamingo`, icon: `🦩` },
+  { type: `penguin`, icon: `🐧` },
+];
+
+export default function List() {
+  const [list, setList] = useState(animals);
+  const [size, setSize]= useState('25px')
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setList((list) => {
+        
+        const inactiveItems = list.filter((item) => !item.active);
+
+        if (inactiveItems.length === 0) {
+          clearInterval(intervalId); 
+          return list;
+        }
+
+        
+        const randomIndex = Math.floor(Math.random() * inactiveItems.length);
+        const randomItem = inactiveItems[randomIndex];
+        console.log(`Random item : ${randomItem.type} ${randomItem.icon}`);
+        const updatedList = list.map((item) =>
+        
+          item === inactiveItems[randomIndex] ? { ...item, active: true } : item
+          
+        );
+        
+
+        return updatedList;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId); 
+  }, []);
+
+  return (
+    <table border="1">
+      <thead>
+        <tr>
+          <th style={{fontSize:size}}>Name</th>
+          <th style={{fontSize:size}}>Icons</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((item, index) => (
+          <tr key={index} className={item.active ? "active" : ""}>
+            <td style={{fontSize:size}}>{item.type}</td>
+            <td style={{fontSize:size}}>{item.icon}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
